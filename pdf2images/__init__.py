8b65f6729c26524ecc036fdaca17d22c309980a7
Saosing <104691783+Saosing@users.noreply.github.com>
2022-06-19 18:56:30 +0000

    Basic version

--- pdf2images/__init__.py
+++ pdf2images/__init__.py
@@ -4,3 +4,7 @@ from .pdf2images import (
     pdf_data_to_thumbnails,
     pdf_data2text,
     get_num_pages_given_path,
+    pdf_data_to_thumbnails_by_qpdf,
+    pdf_data_to_thumbnails_by_imagemagick,
+    pdf_data_to_thumbnails_by_preview_generator,
+)
