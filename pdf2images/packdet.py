2cef2bb383b076f4273efc0b835c79db2613673b
Saosing <104691783+Saosing@users.noreply.github.com>
2022-06-26 14:09:51 +0000

    Add null checks

--- pdf2images/packdet.py
+++ pdf2images/packdet.py
@@ -1,2 +1,8 @@
 
 """Detect if system packages are installed and give friendly installation instruction if not."""
+
+import platform
+import distro
+from loguru import logger
+import os
+import json
