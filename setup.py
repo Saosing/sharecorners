ff867fa344394bb0ad2967e78cdb8b5c8a9b0b22
Saosing <104691783+Saosing@users.noreply.github.com>
2023-02-13 08:23:30 +0000

    Basics

--- setup.py
+++ setup.py
@@ -13,3 +13,7 @@ setuptools.setup(
     name="pdf2images",
     version="0.0.6",
     author="Xinyu Zhou",
+    author_email="zxytim@gmail.com",
+    description="Convert PDF file to image files ROBUSTLY.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
