61efb105eaa8495b86ed5acc926646f3c4b2631c
Saosing <104691783+Saosing@users.noreply.github.com>
2023-02-21 13:54:50 +0000

    Tox circleci test

--- tests/test_pdf2images.py
+++ tests/test_pdf2images.py
@@ -5,3 +5,10 @@ import os
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 ASSETS_DIR = os.path.join(THIS_DIR, "assets")
+
+
+class TestPDF2Images(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pdf_path = os.path.join(ASSETS_DIR, "Sequence Modeling 2019-04.pdf")
+        with open(cls.pdf_path, "rb") as f:
